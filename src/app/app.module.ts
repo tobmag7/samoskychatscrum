2c776e127d41a8f1bf181b109c0441f790f70a45
Oshodi Tobiloba <tobmag1@gmail.com>
2019-11-06 14:25:57 +0100

    final lab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ScrumboardComponent } from './scrumboard/scrumboard.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AuthGuard } from './auth.guard';
+import { ChangeroleGuard } from './changerole.guard';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateprojectComponent } from './createproject/createproject.component';
+import { ChangeroleComponent } from './changerole/changerole.component';
 
 
 @NgModule({
@@ -19,15 +24,20 @@ import { AuthGuard } from './auth.guard';
     SignupComponent,
     LoginComponent,
     ScrumboardComponent,
-    HomepageComponent
+    HomepageComponent,
+    CreateprojectComponent,
+    ChangeroleComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule,
+    BrowserAnimationsModule
   ],
-  providers: [AuthGuard],
+  providers: [AuthGuard, ChangeroleGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
